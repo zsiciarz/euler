78167cf2374227bc871dc360d41115393a419896
Zbigniew Siciarz <antyqjon@gmail.com>
2012-08-12 22:19:39 +0200

    Added 74.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Problem112 (solution)
+import Problem74 (solution)
 
 main :: IO ()
 main = solution
---
+++ src/Problem74.hs
@@ -0,0 +1,37 @@
+
+module Problem74 where
+
+import Common (digits)
+
+factorials :: [Int]
+factorials = scanl (*) 1 [1..10]
+
+factorial :: Int -> Int
+factorial n = factorials !! n
+
+facDigits :: Int -> Int
+facDigits x = sum $ map factorial $ digits x
+
+facChain :: Int -> [Int]
+facChain = iterate facDigits
+
+loopElements :: [Int]
+loopElements = [1, 2, 145, 169, 871, 872, 1454, 40585, 45361, 45362, 363601]
+
+chainLength :: Int -> Int
+chainLength n = length chain + x where
+    (chain, rest) = break (`elem` loopElements) $ facChain n
+    x = case head rest of
+            169 -> 3
+            871 -> 2
+            872 -> 2
+            1454 -> 3
+            45361 -> 2
+            45362 -> 2
+            363601 -> 3
+            _ -> 1
+
+solution :: IO ()
+solution = do
+    print $ length $ filter (== 60) $ map chainLength [1..1000000]
+
