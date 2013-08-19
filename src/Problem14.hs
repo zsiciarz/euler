7c83d01b6be49dc48954bcce51865f4ed4a5ec75
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-19 14:12:27 +0200

    Removed unused code from 14.

--- src/Problem14.hs
+++ src/Problem14.hs
@@ -1,12 +1,6 @@
-
 module Problem14 where
 
 import Data.List
-
-collatz_length :: Int -> Int
-collatz_length 1 = 1
-collatz_length n = 1 + collatz_length(next_value)
-               where next_value = if even n then n `div` 2 else 3 * n + 1
 
 collatzLength :: Int -> Int
 collatzLength = (map collatz [0..] !!) where
@@ -14,10 +8,8 @@ collatzLength = (map collatz [0..] !!) where
     collatz n = 1 + collatzLength(nextValue) where
         nextValue = if even n then n `div` 2 else 3 * n + 1
 
-
 lengths :: [Int]
 lengths = map collatzLength [1..1000000]
--- lengths = map collatz_length [1..1000000]
 
 solution :: IO ()
 solution = do
