77c83f6a405870250600f8c70426ce258738135e
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-20 23:11:43 +0200

    Brute-forcing 60 is too slow.

--- src/Problem60.hs
+++ src/Problem60.hs
@@ -1,6 +1,11 @@
 module Problem60 where
 
+-- possibly works, too slow
+
 import Data.Numbers.Primes (primes, isPrime)
+
+primes' :: Integral a => [a]
+primes' = take 10000 primes
 
 concatNum :: Integral a => a -> a -> a
 concatNum a b = a * 10 ^ p + b where
@@ -9,6 +14,24 @@ concatNum a b = a * 10 ^ p + b where
 checkPrimes :: Integral a => a -> a -> Bool
 checkPrimes a b = (isPrime $ concatNum a b) && (isPrime $ concatNum b a)
 
+concatenablePrimes :: Integral a => [a] -> [[a]]
+concatenablePrimes xs = [[a, b, c, d, e] | a <- xs
+                                         , b <- dropWhile (< a) xs
+                                         , c <- dropWhile (< b) xs
+                                         , d <- dropWhile (< c) xs
+                                         , e <- dropWhile (< d) xs
+                                         , checkPrimes a b
+                                         , checkPrimes a c
+                                         , checkPrimes a d
+                                         , checkPrimes a e
+                                         , checkPrimes b c
+                                         , checkPrimes b d
+                                         , checkPrimes b e
+                                         , checkPrimes c d
+                                         , checkPrimes c e
+                                         , checkPrimes d e
+                                         ]
+
 solution :: IO ()
 solution = do
-    print $ checkPrimes 109 673
+    print $ minimum $ map sum (concatenablePrimes primes')
