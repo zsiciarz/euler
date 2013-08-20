471a7cc77c87bf5032859f9a8d54cacb84239b07
Zbigniew Siciarz <antyqjon@gmail.com>
2013-08-20 21:39:58 +0200

    Added a function to check concatenated primes.

--- src/Problem60.hs
+++ src/Problem60.hs
@@ -6,6 +6,9 @@ concatNum :: Integral a => a -> a -> a
 concatNum a b = a * 10 ^ p + b where
         p = floor $ 1.0 + logBase (10.0::Double) (fromIntegral b)
 
+checkPrimes :: Integral a => a -> a -> Bool
+checkPrimes a b = (isPrime $ concatNum a b) && (isPrime $ concatNum b a)
+
 solution :: IO ()
 solution = do
-    print $ isPrime $ concatNum 109 673
+    print $ checkPrimes 109 673
