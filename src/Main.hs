a0d6eb695ee20f7526433c2e7df684b288caef01
Zbigniew Siciarz <antyqjon@gmail.com>
2013-12-14 20:46:23 +0100

    Even better approach.

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,5 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> putStrLn "Usage: euler <PROBLEM NUMBER>"
-        _  -> do
-            let problem = args !! 0
-            runSolution problem
+        []          -> putStrLn "Usage: euler <PROBLEM NUMBER>"
+        (problem:_) -> runSolution problem
