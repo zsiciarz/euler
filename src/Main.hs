4d4eb5142d8d155dd9cb4c3e24dfd44a87fbf985
Zbigniew Siciarz <antyqjon@gmail.com>
2013-12-14 20:45:02 +0100

    More idiomatic args handling.

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,8 @@ import Solutions (runSolution)
 main :: IO ()
 main = do
     args <- getArgs
-    if length args == 0
-        then putStrLn "Usage: euler <PROBLEM NUMBER>"
-        else do
+    case args of
+        [] -> putStrLn "Usage: euler <PROBLEM NUMBER>"
+        _  -> do
             let problem = args !! 0
             runSolution problem
