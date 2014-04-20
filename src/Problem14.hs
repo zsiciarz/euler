fcd0f3ae85a13aa853085061bcdce3d04cafe60e
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-20 19:25:40 +0200

    Replaced lambda with a section.

--- src/Problem14.hs
+++ src/Problem14.hs
@@ -7,7 +7,7 @@ collatzLength 1 x = x
 collatzLength n x = collatzLength (if even n then n `div` 2 else 3 * n + 1) (x + 1)
 
 lengths :: V.Vector Int
-lengths = V.map (\n -> collatzLength n 1) $ V.enumFromN 1 1000000
+lengths = V.map (`collatzLength` 1) $ V.enumFromN 1 1000000
 
 solution14 :: IO ()
 solution14 = do
