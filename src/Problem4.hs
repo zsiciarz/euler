fc146d984db5feca9a01dc99c60111256b882dea
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-20 14:40:50 +0200

    Redundant $.

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -6,7 +6,7 @@ isPalindrome x = show x == reverse (show x)
 
 solution4 :: IO ()
 solution4 = do
-    print $ maximum $ [x * y | x <- [100..999]
-                             , y <- [100..999]
-                             , isPalindrome $ x * y
-                             ]
+    print $ maximum [x * y | x <- [100..999]
+                           , y <- [100..999]
+                           , isPalindrome $ x * y
+                           ]
