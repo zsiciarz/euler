206a049fe23c5d41d4c1ed4430efa8cdfac2c143
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-20 15:52:21 +0200

    Simplified Collatz length calculation.

--- src/Problem14.hs
+++ src/Problem14.hs
@@ -2,15 +2,13 @@ module Problem14 where
 
 import qualified Data.Vector.Unboxed as V
 
-collatzLength :: Int -> Int
-collatzLength = (map collatz [0..] !!) where
-    collatz 1 = 1
-    collatz n = 1 + collatzLength(nextValue) where
-        nextValue = if even n then n `div` 2 else 3 * n + 1
+collatzLength :: Int -> Int -> Int
+collatzLength 1 x = x
+collatzLength n x = collatzLength (if even n then n `div` 2 else 3 * n + 1) (x + 1)
 
 lengths :: V.Vector Int
-lengths = V.map collatzLength $ V.enumFromN 1 1000000
+lengths = V.map (\n -> collatzLength n 1) $ V.enumFromN 1 1000000
 
 solution14 :: IO ()
 solution14 = do
-    print $ V.maxIndex lengths
+    print $ 1 + V.maxIndex lengths
