40d979a4c25853ac09c445559fc77281a8fa7973
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-20 14:39:38 +0200

    Moved palindrome checking inside list comprehension.

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -6,6 +6,7 @@ isPalindrome x = show x == reverse (show x)
 
 solution4 :: IO ()
 solution4 = do
-    print $ maximum $ filter isPalindrome [x*y | x <- [100..999]
-                                               , y <- [100..999]
-                                               ]
+    print $ maximum $ [x * y | x <- [100..999]
+                             , y <- [100..999]
+                             , isPalindrome $ x * y
+                             ]
