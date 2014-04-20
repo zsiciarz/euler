39247d5f98927c5ff503755f6369becb8c6df3fa
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-20 15:39:15 +0200

    Fixed actual max integer.

--- src/Problem14.hs
+++ src/Problem14.hs
@@ -9,7 +9,7 @@ collatzLength = (map collatz [0..] !!) where
         nextValue = if even n then n `div` 2 else 3 * n + 1
 
 lengths :: V.Vector Int
-lengths = V.map collatzLength $ V.enumFromN 1 10000
+lengths = V.map collatzLength $ V.enumFromN 1 1000000
 
 solution14 :: IO ()
 solution14 = do
