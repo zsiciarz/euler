e019b1d769cd0249b11b11a545c3c738c345216d
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-21 18:13:28 +0200

    Refactored 12 in terms of numDivisors.

--- src/Common.hs
+++ src/Common.hs
@@ -1,6 +1,9 @@
 
 module Common where
 
+import Data.List (group)
+import Data.Numbers.Primes (primeFactors)
+
 digits :: Integral a => a -> [a]
 digits 0 = []
 digits x = r : digits q where
@@ -20,3 +23,10 @@ fibs = map fst $ iterate (\(a,b) -> (b,a+b)) (0,1)
 
 fib :: Integral a => Int -> a
 fib n = fibs !! n
+
+--primePowers :: Integral a => a -> [(a, a)]
+primePowers n = [(head x, length x) | x <- group $ primeFactors n]
+
+divisors n = map product $ sequence [take (k+1) $ iterate (p*) 1 | (p,k) <- primePowers n]
+
+numDivisors n = product [k+1 | (_,k) <- primePowers n]
--- src/Problem12.hs
+++ src/Problem12.hs
@@ -1,29 +1,10 @@
 
 module Problem12 where
 
-import Data.Numbers.Primes
-import Data.List (nub, tails)
-
 import Figurate (triangulars)
 
-
-combinations :: Int -> [a] -> [[a]]
-combinations 0 _  = [ [] ]
-combinations n xs = [ y:ys | y:xs' <- tails xs
-                           , ys <- combinations (n-1) xs'
-                           ]
-
-totalCombinations :: [a] -> [[a]]
-totalCombinations xs = worker (length xs) xs
-                where worker 0 _ = []
-                      worker k xs' = combinations k xs' ++ (worker (k-1) xs')
-
-divisors :: Int -> [Int]
-divisors n = nub $ map product $ totalCombinations $ primeFactors n
-
-countDivisors :: Int -> Int
-countDivisors n = 1 + length (divisors n)
+import Common (numDivisors)
 
 solution12 :: IO ()
 solution12 = do
-    print $ head $ dropWhile (\x -> countDivisors x < 500) triangulars
+    print $ head $ dropWhile (\x -> numDivisors x < 500) triangulars
