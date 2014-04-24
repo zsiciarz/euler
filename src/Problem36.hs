8935e7abc1aa5bbd2ba55bf4f33c4d1c097f6f72
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-24 23:18:10 +0200

    Simplified 36 with digitsBase.

--- src/Problem36.hs
+++ src/Problem36.hs
@@ -1,20 +1,15 @@
 
 module Problem36 where
 
-import Common (digits)
-
-decToBin :: Int -> [Int]
-decToBin x = reverse $ decToBin' x where
-    decToBin' 0 = []
-    decToBin' y = let (a,b) = quotRem y 2 in b : decToBin' a
+import Common (digits, digitsBase)
 
 palindromic10 :: Int -> Bool
 palindromic10 x =  ds == reverse ds where
     ds = digits x
 
 palindromic2 :: Int -> Bool
-palindromic2 x = b == reverse b where
-    b = decToBin x
+palindromic2 x = ds == reverse ds where
+    ds = digitsBase 2 x
 
 solution36 :: IO ()
 solution36 = do
