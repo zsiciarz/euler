de7680862b3feb5c040801b7f8e42dd3284a474d
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-24 22:56:58 +0200

    Refactored 35 in terms of digits/undigits.

--- src/Problem35.hs
+++ src/Problem35.hs
@@ -4,6 +4,8 @@ module Problem35 where
 import Data.Numbers.Primes
 import Data.List
 
+import Common (digits, undigits)
+
 revJoin :: ([a], [a]) -> [a]
 revJoin (x, y) = y ++ x
 
@@ -11,7 +13,7 @@ rotations :: [a] -> [[a]]
 rotations l = drop 1 $ map revJoin (inits l `zip` tails l)
 
 isCircular :: Int -> Bool
-isCircular n = all isPrime (map read (rotations (show n)))
+isCircular n = all isPrime (map undigits (rotations (digits n)))
 
 solution35 :: IO ()
 solution35 = do
