43513acb0827fab5969f725470f1eb870818a15a
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-24 23:06:23 +0200

    Cons is better than concatenation.

--- src/Problem36.hs
+++ src/Problem36.hs
@@ -7,7 +7,7 @@ decToBin :: Int -> [Int]
 decToBin x = reverse $ decToBin' x
         where
         decToBin' 0 = []
-        decToBin' y = let (a,b) = quotRem y 2 in [b] ++ decToBin' a
+        decToBin' y = let (a,b) = quotRem y 2 in b : decToBin' a
 
 palindromic10 :: Int -> Bool
 palindromic10 x =  ds == reverse ds where
