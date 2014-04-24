c3bfe1393c290beba8ecf2f8de26f048bc6ed2ed
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-24 23:07:10 +0200

    More consistent indentation.

--- src/Problem36.hs
+++ src/Problem36.hs
@@ -4,17 +4,17 @@ module Problem36 where
 import Common (digits)
 
 decToBin :: Int -> [Int]
-decToBin x = reverse $ decToBin' x
-        where
-        decToBin' 0 = []
-        decToBin' y = let (a,b) = quotRem y 2 in b : decToBin' a
+decToBin x = reverse $ decToBin' x where
+    decToBin' 0 = []
+    decToBin' y = let (a,b) = quotRem y 2 in b : decToBin' a
 
 palindromic10 :: Int -> Bool
 palindromic10 x =  ds == reverse ds where
     ds = digits x
 
 palindromic2 :: Int -> Bool
-palindromic2 x = b == reverse b where b = decToBin x
+palindromic2 x = b == reverse b where
+    b = decToBin x
 
 solution36 :: IO ()
 solution36 = do
