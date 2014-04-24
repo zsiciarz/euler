addc301e75c4e794a3cbe55e7244c33d62f3c08a
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-24 22:22:30 +0200

    Whitespace fix in 28.

--- src/Problem28.hs
+++ src/Problem28.hs
@@ -2,13 +2,13 @@
 module Problem28 where
 
 sumDiagonals :: Integer -> (Integer, Integer)
-sumDiagonals 1 = (1,1)
+sumDiagonals 1 = (1, 1)
 sumDiagonals level = (prevSum + prevValue + step
-                              + prevValue + 2*step
-                              + prevValue + 3*step
-                              + prevValue + 4*step, prevValue + 4*step)
-        where (prevSum, prevValue) = sumDiagonals (level - 1)
-              step = 2 * (level - 1)
+                              + prevValue + 2 * step
+                              + prevValue + 3 * step
+                              + prevValue + 4 * step, prevValue + 4 * step)
+    where (prevSum, prevValue) = sumDiagonals (level - 1)
+          step = 2 * (level - 1)
 
 solution28 :: IO ()
 solution28 = do
