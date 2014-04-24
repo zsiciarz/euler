d8782c2772f0313431674362a7fe8f7789a2e535
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-24 22:24:09 +0200

    Simplified additions in 28.

--- src/Problem28.hs
+++ src/Problem28.hs
@@ -3,10 +3,7 @@ module Problem28 where
 
 sumDiagonals :: Integer -> (Integer, Integer)
 sumDiagonals 1 = (1, 1)
-sumDiagonals level = (prevSum + prevValue + step
-                              + prevValue + 2 * step
-                              + prevValue + 3 * step
-                              + prevValue + 4 * step, prevValue + 4 * step)
+sumDiagonals level = (prevSum + 4 * prevValue + 10 * step, prevValue + 4 * step)
     where (prevSum, prevValue) = sumDiagonals (level - 1)
           step = 2 * (level - 1)
 
