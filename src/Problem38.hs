46a1ff49c2a22f7b761c7a03f86f2ca98e785754
Zbigniew Siciarz <antyqjon@gmail.com>
2014-04-26 22:55:03 +0200

    More readable definition of makeNumber.

--- src/Problem38.hs
+++ src/Problem38.hs
@@ -4,14 +4,13 @@ module Problem38 where
 
 import Data.List ( inits, sort)
 
-concatenate :: [Int] -> String
-concatenate = concatMap show
-
 isPandigital :: String -> Bool
 isPandigital s = sort s == "123456789"
 
 makeNumber :: Int -> String
-makeNumber x = last $ takeWhile (\s -> length s < 10) $ map concatenate $ inits $ zipWith (*) (repeat x) [1..]
+makeNumber x =  maxNumber . map (concatMap show) . inits $ products where
+    products = zipWith (*) (repeat x) [1..]
+    maxNumber = last . takeWhile (\s -> length s < 10)
 
 solution38 :: IO ()
 solution38 = do
