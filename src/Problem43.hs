cd83a87ab59133ade9b20ddc7fd8cd3a37bf80ef
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-08 21:36:32 +0200

    Pattern matching on digits instead of indexing.

--- src/Problem43.hs
+++ src/Problem43.hs
@@ -9,16 +9,7 @@ pandigitalDigits = filter (\s -> head s /= 0) $ permutations [0..9]
 
 solution43 :: IO ()
 solution43 = do
-    print $ sum [ undigits $ reverse d | d <- pandigitalDigits
-                                  , let d2  = d !! 1
-                                  , let d3  = d !! 2
-                                  , let d4  = d !! 3
-                                  , let d5  = d !! 4
-                                  , let d6  = d !! 5
-                                  , let d7  = d !! 6
-                                  , let d8  = d !! 7
-                                  , let d9  = d !! 8
-                                  , let d10 = d !! 9
+    print $ sum [ undigits $ reverse ds | ds@(_:d2:d3:d4:d5:d6:d7:d8:d9:d10:_) <- pandigitalDigits
                                   , (100*d8 + 10*d9 + d10) `mod` 17 == 0
                                   , (100*d7 + 10*d8 + d9) `mod` 13 == 0
                                   , (100*d6 + 10*d7 + d8) `mod` 11 == 0
