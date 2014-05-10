83faf57c590b9a5f7341b0a7bcd9e839287d3114
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-10 21:54:17 +0200

    Removed unnecessary let bindings in 47.

--- src/Problem47.hs
+++ src/Problem47.hs
@@ -9,12 +9,9 @@ countFactors = length . nub . primeFactors
 
 solution47 :: IO ()
 solution47 = do
-    print $ head [ x | x <- [1..]
-                     , let y = x + 1
-                     , let z = x + 2
-                     , let t = x + 3
-                     , countFactors x == 4
-                     , countFactors y == 4
-                     , countFactors z == 4
-                     , countFactors t == 4
-                     ]
+    print $ head [x | x <- [1..]
+                    , countFactors x == 4
+                    , countFactors (x + 1) == 4
+                    , countFactors (x + 2) == 4
+                    , countFactors (x + 3) == 4
+                    ]
