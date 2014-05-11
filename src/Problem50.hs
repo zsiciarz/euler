aacfee7e30581626be89172839de5ea983758184
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-11 10:56:27 +0200

    Whitespace fix in 50.

--- src/Problem50.hs
+++ src/Problem50.hs
@@ -7,10 +7,10 @@ import Data.List (maximumBy)
 
 solution50 :: IO ()
 solution50 = do
-    print $ fst $ maximumBy (comparing snd) [ (s, length xs) | i <- [0..1000]
-                                                             , j <- [i..1000]
-                                                             , let xs = take j $ drop i primes
-                                                             , let s = sum xs
-                                                             , s < 1000000
-                                                             , isPrime s
-                                                             ]
+    print $ fst $ maximumBy (comparing snd) [(s, length xs) | i <- [0..1000]
+                                                            , j <- [i..1000]
+                                                            , let xs = take j $ drop i primes
+                                                            , let s = sum xs
+                                                            , s < 1000000
+                                                            , isPrime s
+                                                            ]
