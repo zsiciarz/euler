8c344204a5880039f13fdd27e27c52da381aca77
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-11 10:57:30 +0200

    Whitespace fix in 51.

--- src/Problem51.hs
+++ src/Problem51.hs
@@ -10,21 +10,21 @@ replaceDigit :: [Int] -> Int -> Int -> [Int]
 replaceDigit digs digit replacement = [if d == digit then replacement else d | d <- digs]
 
 findReplacements :: [Int] -> Int -> [Int]
-findReplacements digs digit = [ num | r <- [0..9]
-                                    , let digs' = replaceDigit digs digit r
-                                    , let num = undigits digs'
-                                    , last digs' /= 0
-                                    , isPrime num
-                                    ]
+findReplacements digs digit = [num | r <- [0..9]
+                                   , let digs' = replaceDigit digs digit r
+                                   , let num = undigits digs'
+                                   , last digs' /= 0
+                                   , isPrime num
+                                   ]
 
 solution51 :: IO ()
 solution51 = do
-    print $ head [ head replacements | n <- primes
-                                     , let digs = digits n
-                                     , let rest = digs \\ nub digs
-                                     , length rest > 0
-                                     , r <- rest
-                                     , let replacements = findReplacements digs r
-                                     , length replacements == 8
-                                     ]
+    print $ head [head replacements | n <- primes
+                                    , let digs = digits n
+                                    , let rest = digs \\ nub digs
+                                    , length rest > 0
+                                    , r <- rest
+                                    , let replacements = findReplacements digs r
+                                    , length replacements == 8
+                                    ]
 
