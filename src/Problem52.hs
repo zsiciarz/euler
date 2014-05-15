5af4530c7f6860fae496b55a4aa9b405acae076f
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-15 23:40:50 +0200

    Use applicative style for sameDigits check in 52.

--- src/Problem52.hs
+++ src/Problem52.hs
@@ -1,18 +1,16 @@
 
 module Problem52 where
 
+import Control.Applicative
 import Data.List
 import Common (digits)
 
 sameDigits :: Integer -> Integer -> Bool
 sameDigits m n = sort (digits m) == sort (digits n)
 
+sameDigits' :: Integer -> Integer -> Bool
+sameDigits' n x = sameDigits x (n*x)
+
 solution52 :: IO ()
 solution52 = do
-    print $ head [x | x <- [1..]
-                    , sameDigits x (2*x)
-                    , sameDigits x (3*x)
-                    , sameDigits x (4*x)
-                    , sameDigits x (5*x)
-                    , sameDigits x (6*x)
-                    ]
+    print $ head [x | x <- [1..], and $ sameDigits' <$> [2..6] <*> [x]]
