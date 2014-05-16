d2313aae8224d226c3082ce24147b7d46f111b9c
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-16 22:29:12 +0200

    Simplified solution to 53.

--- src/Problem53.hs
+++ src/Problem53.hs
@@ -9,8 +9,4 @@ combinations n r = factorials !! n `div` (factorials !! r * factorials !! (n-r))
 
 solution53 :: IO ()
 solution53 = do
-    print $ length [c | n <- [1..100]
-                      , r <- [1..n]
-                      , let c = combinations n r
-                      , c > 1000000
-                      ]
+    print $ length $ filter (>1000000) [combinations n r | n <- [1..100], r <- [1..n]]
