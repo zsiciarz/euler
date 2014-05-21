3de6bc7006643dca1f901c4b424c741703bf2f1f
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-21 17:56:59 +0200

    Simplified 56 by using digits.

--- src/Problem56.hs
+++ src/Problem56.hs
@@ -1,13 +1,13 @@
 
 module Problem56 where
 
-import Data.Char
+import Common (digits)
 
 numbers :: [Integer]
 numbers = [ a^b | a <- [1..99], b <- [1..99] ]
 
 digitalSum :: Integer -> Integer
-digitalSum n = sum $ map (fromIntegral . digitToInt) $ show n
+digitalSum n = sum $ digits n
 
 solution56 :: IO ()
 solution56 = do
