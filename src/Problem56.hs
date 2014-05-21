aa985b68e12b1e1a30b34bf0c23de7dab2f80a98
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-21 17:58:52 +0200

    Moved list comprehension in-place.

--- src/Problem56.hs
+++ src/Problem56.hs
@@ -3,9 +3,6 @@ module Problem56 where
 
 import Common (digits)
 
-numbers :: [Integer]
-numbers = [ a^b | a <- [1..99], b <- [1..99] ]
-
 solution56 :: IO ()
 solution56 = do
-    print $ maximum $ map (sum . digits) numbers
+    print $ maximum $ map (sum . digits) [a^b | a <- [1..99], b <- [1..99]]
