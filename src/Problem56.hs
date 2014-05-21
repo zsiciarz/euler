0b7f0c5035a4078425a12033d85b407d9b5d9e53
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-21 17:57:39 +0200

    Inlined digitalSum function.

--- src/Problem56.hs
+++ src/Problem56.hs
@@ -6,9 +6,6 @@ import Common (digits)
 numbers :: [Integer]
 numbers = [ a^b | a <- [1..99], b <- [1..99] ]
 
-digitalSum :: Integer -> Integer
-digitalSum n = sum $ digits n
-
 solution56 :: IO ()
 solution56 = do
-    print $ maximum $ map digitalSum numbers
+    print $ maximum $ map (sum . digits) numbers
