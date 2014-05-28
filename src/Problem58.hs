07166c4129b76908a5a6e21b906252f4c9b7969d
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-28 15:08:33 +0200

    Small syntax improvements in 58.

--- src/Problem58.hs
+++ src/Problem58.hs
@@ -13,7 +13,7 @@ primesCount :: Int -> Int
 primesCount n = counts !! n where
         counts = scanl primesCount' 0 [1..]
         primesCount' acc 0 = acc
-        primesCount' acc n' = acc + (length $ filter isPrime diagonals) where
+        primesCount' acc n' = acc + length (filter isPrime diagonals) where
                 diagonals = [s - 2 * n', s - 4 * n', s - 6 * n']
                 s = (2 * n' + 1)^2
 
@@ -22,5 +22,5 @@ ratio n = fromIntegral (primesCount n) / fromIntegral (diagonalsCount n)
 
 solution58 :: IO ()
 solution58 = do
-    print $ sideLength $ head $ dropWhile (\n -> ratio n > 0.1) [1..]
+    print $ sideLength . head $ dropWhile (\n -> ratio n > 0.1) [1..]
 
