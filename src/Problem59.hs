41dc2d42436f93fa2584cba9e8f86a84bb0c36ac
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-31 23:04:37 +0200

    Implement hasCommonWords for list of char values, not strings.

--- src/Problem59.hs
+++ src/Problem59.hs
@@ -4,7 +4,7 @@ module Problem59 where
 import System.IO
 import Control.Applicative ((<$>))
 import Data.Bits (xor)
-import Data.Char (chr)
+import Data.Char (ord)
 import Data.List (isInfixOf)
 import Data.List.Split (splitOn)
 
@@ -20,11 +20,13 @@ keys = [[a, b, c] | a <- [97..122]
 decipher :: [Int] -> [Int] -> [Int]
 decipher input key = zipWith xor input (concat $ repeat key)
 
-hasCommonWords :: String -> Bool
-hasCommonWords s = "the " `isInfixOf` s && "and " `isInfixOf` s
+hasCommonWords :: [Int] -> Bool
+hasCommonWords s = w1 `isInfixOf` s && w2 `isInfixOf` s where
+    w1 = map ord "the "
+    w2 = map ord "and "
 
 solution59 :: IO ()
 solution59 = do
     withFile "data/cipher1.txt" ReadMode $ \h -> do
         input <- readInput <$> hGetContents h
-        print $ sum $ head $ filter (hasCommonWords . map chr) $ map (decipher input) keys
+        print $ sum $ head $ filter hasCommonWords $ map (decipher input) keys
