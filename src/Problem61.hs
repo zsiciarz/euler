d20d264b6314160f3f421b744abe3523b84018c2
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-01 22:46:36 +0200

    Removed primed definitions of figurate numbers.

--- src/Problem61.hs
+++ src/Problem61.hs
@@ -8,26 +8,8 @@ import Figurate
 takeFourDigits :: Integral a => [a] -> [a]
 takeFourDigits = takeWhile (< 10000) . dropWhile (< 1000)
 
-triangulars' :: Integral a => [a]
-triangulars' = takeFourDigits triangulars
-
-squares' :: Integral a => [a]
-squares' = takeFourDigits squares
-
-pentagonals' :: Integral a => [a]
-pentagonals' = takeFourDigits pentagonals
-
-hexagonals' :: Integral a => [a]
-hexagonals' = takeFourDigits hexagonals
-
-heptagonals' :: Integral a => [a]
-heptagonals' = takeFourDigits heptagonals
-
-octagonals' :: Integral a => [a]
-octagonals' = takeFourDigits octagonals
-
 allFigurates :: Integral a => [[a]]
-allFigurates = [triangulars', squares', pentagonals', hexagonals', heptagonals', octagonals']
+allFigurates = map takeFourDigits [triangulars, squares, pentagonals, hexagonals, heptagonals, octagonals]
 
 allFigurates' :: Integral a => [a]
 allFigurates' = concat allFigurates
