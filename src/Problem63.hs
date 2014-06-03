8190fb0283d1ddc3c0a40580b2adbe6e22a4ee59
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-03 22:14:59 +0200

    One-liner list comprehension.

--- src/Problem63.hs
+++ src/Problem63.hs
@@ -2,11 +2,7 @@
 module Problem63 where
 
 powersNDigit :: Integer -> [Integer]
-powersNDigit n = [ p | x <- [1..9]
-                     , let p = x^n
-                     , p >= 10^(n-1)
-                     , p < 10^n
-                     ]
+powersNDigit n = [p | x <- [1..9], let p = x^n, p >= 10^(n-1), p < 10^n]
 
 solution63 :: IO ()
 solution63 = do
