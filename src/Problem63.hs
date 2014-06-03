bdc65c670a592eac3b3ba21e696954b61e6a6eac
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-03 22:15:32 +0200

    Point-free style in solution to 63.

--- src/Problem63.hs
+++ src/Problem63.hs
@@ -6,4 +6,4 @@ powersNDigit n = [p | x <- [1..9], let p = x^n, p >= 10^(n-1), p < 10^n]
 
 solution63 :: IO ()
 solution63 = do
-    print $ length $ concat $ takeWhile (not . null) $ map powersNDigit [1..]
+    print $ length . concat . takeWhile (not . null) $ map powersNDigit [1..]
