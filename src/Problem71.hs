8d25e2e6ac40e2d551ab9a3fbddfa140626b83f8
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-08 23:00:48 +0200

    One-liner list comprehension in 71.

--- src/Problem71.hs
+++ src/Problem71.hs
@@ -3,7 +3,4 @@ module Problem71 where
 
 solution71 :: IO ()
 solution71 = do
-    print $ maximum [a | b <- [999990..1000000]
-                       , a <- [1..(b-1)]
-                       , b * 3 - a * 7 == 1
-                       ]
+    print $ maximum [a | b <- [999990..1000000], a <- [1..(b - 1)], b * 3 - a * 7 == 1]
