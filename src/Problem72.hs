02df8d2bd9337039acc37b8558a1b00563ba2178
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-09 15:29:18 +0200

    Don't use nub in 72.

--- src/Problem72.hs
+++ src/Problem72.hs
@@ -2,13 +2,16 @@
 
 module Problem72 where
 
-import Data.List (nub)
 import Data.Ratio
 import Data.Numbers.Primes (primeFactors)
+import qualified Data.Set as S
+
+uniqueFactors :: Integer -> [Integer]
+uniqueFactors =  S.toList . S.fromList . primeFactors
 
 phi :: Integer -> Integer
 phi 1 = 1
-phi n = numerator $ (n % 1) * (product $ map (\p -> (1 - 1 % p)) $ nub $ primeFactors n)
+phi n = numerator $ (n % 1) * (product $ map (\p -> (1 - 1 % p)) $ uniqueFactors n)
 
 fareyLength :: Integer -> Integer
 fareyLength 1 = 2
