2f716a25bdbd6575af5c1da893344455536f9bb0
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-22 10:29:40 +0200

    Cleaned up 77.

--- src/Problem77.hs
+++ src/Problem77.hs
@@ -9,8 +9,8 @@ primes' = take 100 primes
 ways :: [Int] -> [Int]
 ways [] = 1 : repeat 0
 ways (c:cs) = n where
-                n = zipWith (+) (ways cs) (replicate c 0 ++ n)
+    n = zipWith (+) (ways cs) (replicate c 0 ++ n)
 
 solution77 :: IO ()
 solution77 = do
-    print $ (length $ takeWhile (< 5000) $ map (ways primes' !!) [1..]) + 1
+    print $ 1 + (length . takeWhile (<5000) . map (ways primes' !!) $ [1..])
