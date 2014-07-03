ef27dadebc359f2ef4935cb42b99c14ddfcb82ec
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-03 20:26:23 +0200

    Indentation fix in 78.

--- src/Problem78.hs
+++ src/Problem78.hs
@@ -5,7 +5,7 @@ module Problem78 where
 ways :: [Int] -> [Int]
 ways [] = 1 : repeat 0
 ways (c:cs) = n where
-                n = zipWith (+) (ways cs) (replicate c 0 ++ n)
+    n = zipWith (+) (ways cs) (replicate c 0 ++ n)
 
 solution78 :: IO ()
 solution78 = do
