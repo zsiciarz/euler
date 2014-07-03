b19e3c7215ff158996ff636b5028738d0234439c
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-03 23:06:49 +0200

    Use unboxed arrays.

--- src/Problem92.hs
+++ src/Problem92.hs
@@ -2,7 +2,7 @@
 module Problem92 where
 
 import Common (digits)
-import Data.Array
+import Data.Array.Unboxed
 
 limit :: Int
 limit = 10000000
@@ -10,7 +10,7 @@ limit = 10000000
 sumSquareDigits :: Int -> Int
 sumSquareDigits = sum . map (^2) . digits
 
-chainCache :: Array Int Int
+chainCache :: UArray Int Int
 chainCache = listArray (1, 7 * 9^2) . map sumSquareDigits $ [1..7 * 9^2]
 
 chain :: Int -> Int
