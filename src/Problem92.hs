cb45efed782ee6dd10dea9c39d223ee9eb7587e5
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-03 23:04:36 +0200

    Precompute chain elements.

--- src/Problem92.hs
+++ src/Problem92.hs
@@ -1,14 +1,23 @@
--- works, brute force
 
 module Problem92 where
 
 import Common (digits)
+import Data.Array
+
+limit :: Int
+limit = 10000000
+
+sumSquareDigits :: Int -> Int
+sumSquareDigits = sum . map (^2) . digits
+
+chainCache :: Array Int Int
+chainCache = listArray (1, 7 * 9^2) . map sumSquareDigits $ [1..7 * 9^2]
 
 chain :: Int -> Int
 chain 1 = 1
 chain 89 = 89
-chain n = chain . sum . map (^2) . digits $ n
+chain n = chain $ chainCache ! n
 
 solution92 :: IO ()
 solution92 = do
-    print $ length $ filter (\s -> chain s == 89) [1..10000000]
+    print . length . filter (\s -> (chain . sumSquareDigits) s == 89) $ [1..limit]
