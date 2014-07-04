c2ed103dc2fce8ac135b4c0c17405796eb5d77a7
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-04 22:08:01 +0200

    Refactored 97 in terms of digits/undigits.

--- src/Problem97.hs
+++ src/Problem97.hs
@@ -1,13 +1,8 @@
--- works, brute force
 
 module Problem97 where
 
-lastDigits :: Integer -> Int -> Integer
-lastDigits x n = lastDigits' x n 0 where
-    lastDigits' _ 0 _ = 0
-    lastDigits' x' n' p = (m * 10^p) + (lastDigits' d (n'-1) (p+1)) where
-            (d, m) = x' `quotRem` 10
+import Common (digits, undigits)
 
 solution97 :: IO ()
 solution97 = do
-    print $ lastDigits (28433 * 2^7830457 + 1) 10
+    print . undigits . take 10 . digits $ 28433 * 2^7830457 + 1
