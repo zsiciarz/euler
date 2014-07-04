619d4a46e977a3c3c92c03c77eeba840e1b0be6a
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-04 22:27:43 +0200

    Use withFile in 99.

--- src/Problem99.hs
+++ src/Problem99.hs
@@ -2,7 +2,7 @@
 
 module Problem99 where
 
-import System.IO
+import System.IO (withFile, IOMode(..), hGetContents)
 import Data.List (maximumBy)
 import Data.List.Split (splitOn)
 import Data.Ord (comparing)
@@ -15,8 +15,8 @@ processLine s = e' * logBase 10.0 b' where
 
 solution99 :: IO ()
 solution99 = do
-    h <- openFile "data/base_exp.txt" ReadMode
-    contents <- hGetContents h
-    let numbers = zip [1..] $ map processLine $ lines contents
-    let m = maximumBy (comparing snd) numbers
-    print $ fst m
+    withFile "data/base_exp.txt" ReadMode $ \h -> do
+        contents <- hGetContents h
+        let numbers = zip [1..] $ map processLine $ lines contents
+        let m = maximumBy (comparing snd) numbers
+        print $ fst m
