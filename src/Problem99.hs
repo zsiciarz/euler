6f0c344bd6f1ccfb95bdff4ef6121fdd222f2fe4
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-04 22:41:04 +0200

    Indentation fix.

--- src/Problem99.hs
+++ src/Problem99.hs
@@ -10,9 +10,9 @@ maximumIndex = fst . maximumBy (comparing snd) . zip [1..]
 
 processLine :: String -> Float
 processLine s = e' * logBase 10.0 b' where
-                b' = read b
-                e' = read e
-                (b, _:e) = break (==',') s
+    b' = read b
+    e' = read e
+    (b, _:e) = break (==',') s
 
 solution99 :: IO ()
 solution99 = do
