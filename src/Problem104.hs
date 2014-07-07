a56c55d917468461bb745e31ceb68a569a568c42
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-07 21:29:01 +0200

    Skip first 2749 Fibonacci numbers.

--- src/Problem104.hs
+++ src/Problem104.hs
@@ -7,9 +7,12 @@ import Common (fibs)
 isPandigital :: String -> Bool
 isPandigital s = sort s == "123456789"
 
+fibs' :: [Integer]
+fibs' = drop 2749 fibs
+
 solution104 :: IO ()
 solution104 = do
-    print $ head [n | (x, n) <- zip fibs [0..]
+    print $ head [n | (x, n) <- zip fibs' [2750..]
                     , let digits = show x
                     , isPandigital $ take 9 digits
                     , isPandigital $ take 9 $ reverse digits
