334fc9560f31c8e43bc32aa7a7f93ebeaf9260a9
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-07 22:15:06 +0200

    Short and sweet monoidal compare implementation.

--- src/Problem124.hs
+++ src/Problem124.hs
@@ -1,7 +1,9 @@
 
 module Problem124 where
 
+import Data.Function (on)
 import Data.List (sortBy)
+import Data.Monoid ((<>))
 import Data.Numbers.Primes (primeFactors)
 import qualified Data.IntSet as S
 import qualified Data.Vector.Unboxed as V
@@ -14,11 +16,7 @@ rad :: Int -> Int
 rad n = rads V.! (n-1)
 
 compareByRad :: Int -> Int -> Ordering
-compareByRad x y
-    | rx == ry = compare x y
-    | otherwise = compare rx ry where
-        rx = rad x
-        ry = rad y
+compareByRad x y = (compare `on` rad) x y <> x `compare` y
 
 e :: Int -> Int
 e n =  sortBy compareByRad [1..100000] !! (n-1)
