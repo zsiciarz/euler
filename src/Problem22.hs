de768eba2fb0729c3a2c5f44141981cf474e0138
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-19 15:26:17 +0200

    A few small stylistic fixes.

--- src/Problem22.hs
+++ src/Problem22.hs
@@ -7,13 +7,13 @@ import Data.List (sort)
 import Data.List.Split (splitOn)
 
 charValue :: Char -> Int
-charValue c = (ord c) - 64 -- (ord 'A') + 1
+charValue c = ord c - 64 -- (ord 'A') + 1
 
-nameValue :: [Char] -> Int
-nameValue name = sum $ map charValue $ filter (`elem` ['A'..'Z']) name
+nameValue :: String -> Int
+nameValue = sum . map charValue . filter (`elem` ['A'..'Z'])
 
-findSolution :: [Char] -> Int
-findSolution s = sum [ i * (nameValue name) | (i, name) <- zip [1..] $ sort (splitOn "," s) ]
+findSolution :: String -> Int
+findSolution s = sum [i * nameValue name | (i, name) <- zip [1..] $ sort (splitOn "," s)]
 
 solution22 :: IO ()
 solution22 = do
