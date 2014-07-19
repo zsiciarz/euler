1429566f95e07158ae30b7632a49e334f1bd27f1
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-19 15:18:23 +0200

    Use cons instead of appending.

--- src/Problem15.hs
+++ src/Problem15.hs
@@ -2,7 +2,7 @@
 module Problem15 where
 
 pascal :: [[Integer]]
-pascal = iterate (\row -> zipWith (+) ([0] ++ row) (row ++ [0])) [1]
+pascal = iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1]
 
 solution15 :: IO ()
 solution15 = do
