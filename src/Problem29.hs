6c35cd860b30c82f89e63e57be6cd5c9eb568591
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-20 06:49:53 +0200

    Use Data.Set in 29.

--- src/Problem29.hs
+++ src/Problem29.hs
@@ -1,11 +1,8 @@
 
 module Problem29 where
 
-import Data.List
-
-terms :: Int -> Int -> [Int]
-terms from to = [x^y | x <- [from..to], y <- [from..to] ]
+import qualified Data.Set as S
 
 solution29 :: IO ()
 solution29 = do
-    print $ length $ nub $ terms 2 100
+    print . S.size . S.fromList $ [a^b | a <- [2..100], b <- [2..100]]
