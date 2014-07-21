4234aba53209814ee908a0d234ba0cc9548190de
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-21 22:56:38 +0200

    Stylistic fixes in 187.

--- src/Problem187.hs
+++ src/Problem187.hs
@@ -1,8 +1,8 @@
 
 module Problem187 where
 
-import Data.Numbers.Primes
+import Data.Numbers.Primes (primeFactors)
 
 solution187 :: IO ()
 solution187 = do
-    print $ length $ filter (\s -> (length $ primeFactors s) == 2) [1..100000000]
+    print . length . filter (\s -> length (primeFactors s) == 2) $ [1..100000000]
