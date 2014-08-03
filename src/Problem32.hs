19a7f27be4f7670af3a19cd58a5743cc07d58260
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-03 12:34:35 +0200

    Fixed whitespace in 32.

--- src/Problem32.hs
+++ src/Problem32.hs
@@ -8,13 +8,13 @@ undigits' :: [Int] -> Int
 undigits' = undigits . reverse
 
 products :: [Int]
-products = [ undigits' r | xs <- permutations [1..9]
-               , i <- [1..5]
-               , j <- [1..8-i]
-               , let (p, rest) = splitAt i xs
-               , let (q, r) = splitAt j rest
-               , undigits' p * undigits' q == undigits' r
-               ]
+products = [undigits' r | xs <- permutations [1..9]
+           , i <- [1..5]
+           , j <- [1..8-i]
+           , let (p, rest) = splitAt i xs
+           , let (q, r) = splitAt j rest
+           , undigits' p * undigits' q == undigits' r
+           ]
 
 solution32 :: IO ()
 solution32 = do
