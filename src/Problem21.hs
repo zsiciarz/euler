b1df51d9c9764281226fee48d0a92f4d611bd631
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-12 20:49:06 +0200

    Solution to 21 using divisors map.

--- src/Problem21.hs
+++ src/Problem21.hs
@@ -1,5 +1,17 @@
 
 module Problem21 where
 
+import qualified Data.Map as M
+
+import Common (divisors)
+
+sumDivisors :: Integral a => a -> a
+sumDivisors x = sum . filter (/= x) $ divisors x
+
+amicablePairs :: Integral a => a -> M.Map a a
+amicablePairs upto = M.filterWithKey (\k v -> k /= v && M.lookup v divisorsMap == Just k) divisorsMap where
+    divisorsMap = foldr (\x mp -> M.insert x (sumDivisors x) mp) M.empty [2..upto]
+
 solution21 :: IO ()
-solution21 = putStrLn "TODO"
+solution21 = do
+    print $ sum . M.keys . amicablePairs $ 10000
