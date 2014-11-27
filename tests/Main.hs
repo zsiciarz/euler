330dc028b2e8831cc87045eb33b083a7952c7bb1
Zbigniew Siciarz <antyqjon@gmail.com>
2014-11-27 15:27:25 +0100

    Second test case.

--- tests/Main.hs
+++ tests/Main.hs
@@ -5,13 +5,19 @@ import System.Exit (exitFailure)
 import Test.HUnit
 
 import Problem1 (solution1)
+import Problem2 (solution2)
 
 testSolution1 :: Test
 testSolution1 = TestCase $ do
     result <- solution1
     assertEqual "test solution for problem 1" (result `mod` 100) 68
 
+testSolution2 :: Test
+testSolution2 = TestCase $ do
+    result <- solution2
+    assertEqual "test solution for problem 2" (result `mod` 100) 32
+
 main :: IO ()
 main = do
-    c <- runTestTT testSolution1
+    c <- runTestTT $ TestList [testSolution1, testSolution2]
     when (errors c /= 0 || failures c /= 0) exitFailure
