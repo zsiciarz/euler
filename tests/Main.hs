17d595ebbf9cfa461cc418f466bfd395fd8addb8
Zbigniew Siciarz <antyqjon@gmail.com>
2014-11-27 15:52:25 +0100

    Removed top-level test case names.

--- tests/Main.hs
+++ tests/Main.hs
@@ -13,13 +13,10 @@ makeTestCase problemNumber solution lastDigits = TestCase $ do
     assertEqual label (result `mod` 100) lastDigits
         where label = "test solution for problem " ++ show problemNumber
 
-testSolution1 :: Test
-testSolution1 = makeTestCase 1 solution1 68
-
-testSolution2 :: Test
-testSolution2 = makeTestCase 2 solution2 32
-
 main :: IO ()
 main = do
-    c <- runTestTT $ TestList [testSolution1, testSolution2]
+    c <- runTestTT $ TestList [
+        makeTestCase 1 solution1 68
+        , makeTestCase 2 solution2 32
+        ]
     when (errors c /= 0 || failures c /= 0) exitFailure
