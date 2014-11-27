488f15c1b4eda5739a0d3c71e273f765b30af5ab
Zbigniew Siciarz <antyqjon@gmail.com>
2014-11-27 14:57:15 +0100

    Dummy test case.

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import Control.Monad (when)
+import System.Exit (exitFailure)
 import Test.HUnit
 
+import Problem1 (solution1)
+
+testSolution1 :: Test
+testSolution1 = TestCase $ assertEqual "test solution for problem 1" 1 1
+
 main :: IO ()
-main = putStrLn "Hello, tests!"
+main = do
+    c <- runTestTT testSolution1
+    when (errors c /= 0 || failures c /= 0) $
+        exitFailure
