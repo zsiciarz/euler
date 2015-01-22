2dabd8feb43a58e28332a96f1eef5db4c4282dd5
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-22 21:06:12 +0100

    Added naive solution for 24.

--- src/Problem24.hs
+++ src/Problem24.hs
@@ -1,5 +1,10 @@
 
 module Problem24 where
 
+import Data.List (permutations, sort)
+import Common (undigits)
+
 solution24 :: IO Integer
-solution24 = return (-1)
+solution24 = do
+    let perms = sort $ permutations [0..9]
+    return $ undigits $ reverse $ perms !! 999999
