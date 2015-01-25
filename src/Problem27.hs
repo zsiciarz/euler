6e7a8281cc980e57ba82fcaecf372122ba24a200
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-25 19:59:10 +0100

    Whitespace fix in 27.

--- src/Problem27.hs
+++ src/Problem27.hs
@@ -9,10 +9,10 @@ formula :: Int -> Int -> Int -> Int
 formula a b n = n^2 + a * n + b
 
 primesByFormula :: Int -> Int -> [Int]
-primesByFormula a b = takeWhile isPrime [formula a b n | n <- [0..] ]
+primesByFormula a b = takeWhile isPrime [formula a b n | n <- [0..]]
 
 pairs :: [((Int, Int), Int)]
-pairs = [ ((a, b), length $ primesByFormula a b) | a <- [-999..999], b <- [-999..999] ]
+pairs = [((a, b), length $ primesByFormula a b) | a <- [-999..999], b <- [-999..999]]
 
 solution27 :: IO Integer
 solution27 = do
