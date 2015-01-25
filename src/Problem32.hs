917d7261f400bebc9ac8ae87f640c3ab22d83246
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-25 19:04:22 +0100

    Precompute undigits in 32.

--- src/Problem32.hs
+++ src/Problem32.hs
@@ -3,10 +3,14 @@ module Problem32 where
 
 import Data.List (permutations)
 import qualified Data.IntSet as S
-import Common (undigits)
+import qualified Data.Map as M
+import Common (digits)
+
+undigits :: M.Map [Int] Int
+undigits = foldr (\x m -> M.insert (digits x) x m) M.empty [1..100000]
 
 undigits' :: [Int] -> Int
-undigits' = undigits . reverse
+undigits' ds = M.findWithDefault 0 ds undigits
 
 products :: [Int]
 products = [undigits' r | xs <- permutations [1..9]
