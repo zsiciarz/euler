724fb8b58a9ed1cce39e83400ffb942423926439
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-25 10:44:31 +0100

    Got rid of local version of divisors in 23.

--- src/Problem23.hs
+++ src/Problem23.hs
@@ -2,20 +2,13 @@
 
 module Problem23 where
 
+import Common (divisors)
+
 limit :: Int
 limit = 28123
 
-divisors :: Int -> [Int]
-divisors x = divisors' 2 [1] x
-    where divisors' d divs n
-            | d^2 > n      = divs
-            | (mod n d)==0 = if (div n d) == d
-                             then divisors' (d+1) (d : divs) n
-                             else divisors' (d+1) (d : (div n d) : divs) n
-            | otherwise    = divisors' (d+1) divs n
-
 isAbundant :: Int -> Bool
-isAbundant x = x < sum (divisors x)
+isAbundant x = (2*x) < sum (divisors x)
 
 abundants :: [Int]
 abundants = filter isAbundant [1..limit]
