2aa61eade78ea996fe50391e50c8c8570c917f6c
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-26 20:40:28 +0100

    Simplified collatzLength definition in 14.

--- src/Problem14.hs
+++ src/Problem14.hs
@@ -2,12 +2,12 @@ module Problem14 where
 
 import qualified Data.Vector.Unboxed as V
 
-collatzLength :: Int -> Int -> Int
-collatzLength 1 x = x
-collatzLength n x = collatzLength (if even n then n `div` 2 else 3 * n + 1) (x + 1)
+collatzLength :: Int -> Int
+collatzLength 1 = 1
+collatzLength n = 1 + collatzLength (if even n then n `div` 2 else 3 * n + 1)
 
 lengths :: V.Vector Int
-lengths = V.map (`collatzLength` 1) $ V.enumFromN 1 1000000
+lengths = V.map collatzLength $ V.enumFromN 1 1000000
 
 solution14 :: IO Integer
 solution14 = do
