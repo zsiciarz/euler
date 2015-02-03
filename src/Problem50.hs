9fb05a491274be5257e35a59f20c4a53ac2e572b
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-02-03 11:40:15 +0100

    Replaced take+drop with vector slicing in 50.

--- src/Problem50.hs
+++ src/Problem50.hs
@@ -4,13 +4,16 @@ module Problem50 where
 import Data.Numbers.Primes (primes, isPrime)
 import Data.Ord (comparing)
 import Data.List (maximumBy)
+import qualified Data.Vector.Unboxed as V
+
+primes' :: V.Vector Int
+primes' = V.fromList $ takeWhile (<1000000) primes
 
 solution50 :: IO Integer
 solution50 = do
-    return $ fst $ maximumBy (comparing snd) [(s, j) | i <- [0..1000]
+    return $ fromIntegral $ fst $ maximumBy (comparing snd) [(s, j) | i <- [0..1000]
                                                             , j <- [i..1000]
-                                                            , let xs = take j $ drop i primes
-                                                            , let s = sum xs
+                                                            , let s = V.sum $ V.slice i j primes'
                                                             , s < 1000000
                                                             , isPrime s
                                                             ]
